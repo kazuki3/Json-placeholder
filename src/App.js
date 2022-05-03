5297561ab3d56d47364a374628507ecd037f6170
kazuki3 <45926663+kazuki3@users.noreply.github.com>
2022-05-03 16:19:25 +0900

    axiosによる通信処理

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./styles.css";
+import axios from "axios";
 
 export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((result) => console.log(result))
+      .catch((error) => console.log(error));
+  };
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=2")
+      .then((result) => console.log(result))
+      .catch((error) => console.log(error));
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>user-1</button>
     </div>
   );
 }
